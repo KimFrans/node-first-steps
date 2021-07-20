94a58ff650b0714496c2003f3c9db422b9ec227f
Kim Frans <kimfrans34@gmail.com>
2021-07-20 12:13:06 +0200

    initial commit

---
+++ greet_xola.js
@@ -0,0 +1,23 @@
+// const greet = require('./greet');
+
+// console.log(greet('Xola'))
+
+// const chalk = require('chalk');
+//import the greet module that is in the current folder
+// const greet = require('./greet');
+
+// const styledMessage = chalk.bgGreen.black(greet('Xola'));
+// console.log(styledMessage)
+
+var figlet = require('figlet');
+
+var greet = require('./greet')
+ 
+figlet(greet('Xola'), function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data)
+});
