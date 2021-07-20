524c3b018a2356ceee407caaf04140db9b80bfe3
Kim Frans <kimfrans34@gmail.com>
2021-07-20 15:38:56 +0200

    chalk and figlet

--- greet_xola.js
+++ greet_xola.js
@@ -2,16 +2,16 @@
 
 // console.log(greet('Xola'))
 
-// const chalk = require('chalk');
-//import the greet module that is in the current folder
-// const greet = require('./greet');
+const chalk = require('chalk');
+// import the greet module that is in the current folder
+const greet = require('./greet');
 
-// const styledMessage = chalk.bgGreen.black(greet('Xola'));
-// console.log(styledMessage)
+const styledMessage = chalk.bgGreen.black(greet('Xola'));
+console.log(styledMessage)
 
 var figlet = require('figlet');
 
-var greet = require('./greet')
+// var greet = require('./greet')
  
 figlet(greet('Xola'), function(err, data) {
     if (err) {
